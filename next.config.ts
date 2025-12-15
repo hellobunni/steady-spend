777004ac13d08ee8f161e3b39ee29bffb188bdbd
hellobunni <jagaestheticdigital@gmail.com>
2025-12-15 13:39:38 -0500

    built blog post out for tools and apps 2026 draft (v2)

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,61 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.youneedabudget.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.everydollar.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.simplifimoney.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.copilot.money',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.monarchmoney.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.nerdwallet.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.rocketmoney.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.empower.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logo.clearbit.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
 };
