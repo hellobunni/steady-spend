ff6a545f66b159db7fbbbe7ec23fc2894ae2ac40
hellobunni <jagaestheticdigital@gmail.com>
2026-01-05 10:03:03 -0500

    updated AuthorBio component

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -298,6 +298,8 @@ export default async function BlogPostPage({ params }: Props) {
           )}
         </div>
 
+        <AuthorBio />
+
           {/* Related Posts */}
           {relatedPosts.length > 0 && (
             <RelatedPosts posts={relatedPosts} />
